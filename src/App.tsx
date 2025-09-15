c0a4bf5251ecd39c56ad972999a3b3c69e487ab4
wldnjs990 <wl990@naver.com>
2025-09-16 00:07:39 +0900

    chore : motion 라이브러리 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import * as motion from 'motion/react-client';
 export default function App() {
-  return <div className="h-10 w-10 bg-amber-200">App</div>;
+  const arr = Array(5).fill(0);
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-10">
+      <div className="flex gap-3">
+        {arr.map((_, idx) => {
+          return <Box key={idx} />;
+        })}
+      </div>
+    </div>
+  );
+}
+
+function Box() {
+  return (
+    <motion.div
+      className="flex h-25 w-25 items-center justify-center rounded-2xl bg-amber-500"
+      whileHover={{ scale: 2 }}
+      transition={{ duration: 0.3 }}
+    >
+      옵션1
+    </motion.div>
+  );
 }
